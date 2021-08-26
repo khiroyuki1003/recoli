796f781da880415cf59ed088862593a750d06c0e
tokorotenn <hk.1003.abc@gmail.com>
2021-08-26 19:23:10 +0900

    Usersテーブル作成

--- db/migrate/20210826101406_devise_create_users.rb
+++ db/migrate/20210826101406_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :precious_word
+      t.date :birth_date, nul: false
 
       ## Recoverable
       t.string   :reset_password_token
