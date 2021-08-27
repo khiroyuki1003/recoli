fd755ce770dce12e4071209d174046f267ac32ce
tokorotenn <hk.1003.abc@gmail.com>
2021-08-27 20:45:44 +0900

    プロフィールのルーティングセット

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions", 
+    registrations: "users/registrations"
+    } 
   root to:'categories#index'
+
+  resources :profiles, only: [:new, :create]
 end
