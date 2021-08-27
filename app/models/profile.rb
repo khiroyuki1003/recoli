7d9c792eeff4b2b61a9b5d83add099763c01d5a2
tokorotenn <hk.1003.abc@gmail.com>
2021-08-27 20:44:31 +0900

    プロフィールモデル作成

---
+++ app/models/profile.rb
@@ -0,0 +1,17 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :nickname, length: {
+       maximum: 20, message:"is invalid. Please enter at least 20 characters"
+      }
+    validates :birth_date
+  end
+
+  validates :nickname, uniqueness: { message: "That nickname is already in use" }
+
+  validates :precious_word, length: {
+     maximum: 100, message:"is invalid. Please enter at least 100 characters"
+    }
+end
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :profile
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname, length: { maximum: 20, message:"is invalid. Please enter at least 20 characters"}
-    validates :birth_date
-  end
-
   validates :password, format: {with: /\A[a-zA-Z0-9]+\z/, message:"is invalid. Half-width alphanumeric only"}
-  validates :precious_word, length: { maximum: 100, message:"is invalid. Please enter at least 100 characters"}
+  
 end
