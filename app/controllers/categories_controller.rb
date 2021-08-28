506c58182b3cb5cd35e776319cf3b54851b8f19c
tokorotenn <hk.1003.abc@gmail.com>
2021-08-28 19:18:57 +0900

    カテゴリ新規作成機能

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
-  # before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, except: :index
+  before_action :get_user, only: [:index, :new, :create]
 
   def index
+    
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @create = Category.new(category_params)
+    if @create.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def get_user
     if user_signed_in?
       @user = User.find(current_user.id)
     end
   end
 
-end
+  def category_params
+    params.require(:category).permit(:category_name).merge(profile: @user.profile)
+  end
+end 
