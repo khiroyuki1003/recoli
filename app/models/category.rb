3bcc3b72f7dfb651ffb40d4174bef94b665eb4f2
tokorotenn <hk.1003.abc@gmail.com>
2021-08-28 20:58:55 +0900

    バリデーション設定/エラーハンドリング

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :profile
+
+  validates :category_name, presence: true, length: {
+    maximum: 20, message:"is invalid. Please enter at least 20 characters"
+   }
 end
