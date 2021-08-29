e6760c6673bb4ee9d85762936ef3a134a3220085
tokorotenn <hk.1003.abc@gmail.com>
2021-08-29 14:51:21 +0900

    プロフィールアクセス制限の修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :create, :show, :edit, :update]
+  before_action :authenticate_user!
   before_action :set_user, only: [:new, :show, :edit, :update]
   before_action :set_profile, only: [:show, :edit, :update]
-  before_action :user_profile_check, only: [:new, :edit, :update]
+  before_action :user_profile_check, only: [:new, :cretae]
+  before_action :profile_access_check, only: [:show, :edit, :update]
+
 
   def new
     @profile = Profile.new
@@ -33,22 +35,29 @@ class ProfilesController < ApplicationController
 
   private
 
-  def profile_params
-    params.require(:profile).permit(:nickname, :precious_word, :birth_date).merge(user: current_user)
-  end
-
   def set_user
     @user = User.find(current_user.id)
   end
 
   def set_profile
-    @profile = Profile.find(@user.profile.id)
+    @profile = Profile.find(params[:id])
   end
 
   def user_profile_check
     if @user.profile.present?
       redirect_to root_path
     end
+  end
+
+  def profile_access_check
+    binding.pry
+    if @profile.id != @user.profile.id
+      redirect_to root_path
+    end
+  end
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :precious_word, :birth_date).merge(user: current_user)
   end
 end
  
