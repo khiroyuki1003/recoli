1d0e58b569dc99f6ea7d702f347353d72e80ddb7
tokorotenn <hk.1003.abc@gmail.com>
2021-08-29 16:55:05 +0900

    アクティブハッシュ作成・アソシエーション設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :profile
+  has_many :lists
 
   validates :category_name, presence: true, length: {
     maximum: 20, message:"is invalid. Please enter at least 20 characters"
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :priolity
 end
