a7270ac35526cdb77ad255e8c0e0bba19e40d3df
tokorotenn <hk.1003.abc@gmail.com>
2021-08-30 14:04:21 +0900

    リストテストコード作成

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @list = FactoryBot.build(:list)
+  end
+
+  context 'リスト作成できる時' do
+    it '全ての項目が正しく入力されていれば作成できる' do
+      expect(@list).to be_valid
+    end
+    it 'list_detailが空でも作成できる' do
+      list_detail = ""
+      expect(@list).to be_valid
+    end
+    it 'priorityが選択されていなくても作成できる' do
+      priority_id = 1
+      expect(@list).to be_valid
+    end
+  end
+
+  context 'リスト作成できない時' do
+    it 'list_titleが空だと保存できない' do
+      @list.list_title = ''
+      @list.valid?
+      expect(@list.errors.full_messages).to include("List title can't be blank")
+    end
+  end
 end
+
+
+# bundle exec rspec spec/models/list_spec.rb 
+
+# binding.pry
+
+# @list.errors.full_messages
