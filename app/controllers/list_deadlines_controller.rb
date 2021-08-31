84e3a36e584307cd667c8d21872e871ef6045c1f
tokorotenn <hk.1003.abc@gmail.com>
2021-08-31 11:43:55 +0900

    full_calenderプラグイン読み込み完了

---
+++ app/controllers/list_deadlines_controller.rb
@@ -0,0 +1,13 @@
+class ListDeadlinesController < ApplicationController
+  before_action :get_user
+
+  def index
+  end
+
+  private
+  def get_user
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    end
+  end
+end
