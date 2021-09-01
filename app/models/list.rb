3a1a972119dcec199c9e580de338d75033ff4467
tokorotenn <hk.1003.abc@gmail.com>
2021-09-01 14:18:44 +0900

    リストモデルにバリデーション戻す（編集機能のため））

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,6 +4,8 @@ class List < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :priority
 
- 
+  validates :list_title, presence: true, length: {
+    maximum: 100, message:"is invalid. Please enter at least 100 characters"
+   }
 end
  
