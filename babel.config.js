69781772f26ddaeb96060a66a7784706f541624d
tokorotenn <hk.1003.abc@gmail.com>
2021-09-02 20:56:43 +0900

    修正

--- babel.config.js
+++ babel.config.js
@@ -68,17 +68,18 @@ module.exports = function(api) {
         }
       ]
       [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-           "loose": true
-        }
-      ],
-      [
         '@babel/plugin-proposal-private-methods',
         {
             "loose": true
         }
       ]
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+           "loose": true
+        }
+      ]
     ].filter(Boolean)
   }
 }
+
