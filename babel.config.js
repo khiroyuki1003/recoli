750dda0aa21b59eb02647f3fbda8c4d640c0d18b
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 11:35:48 +0900

    Revert "Merge pull request #29 from khiroyuki1003/babel.config-修正"
    This reverts commit 623105b4fdd245354b23517ece9afa9f6b73ec14, reversing
    changes made to 291198ee76a4325f42702e8bf6e82ddc6b304797.

--- babel.config.js
+++ babel.config.js
@@ -73,11 +73,6 @@ module.exports = function(api) {
            "loose": true
         }
       ]
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-            "loose": true
-        }]
     ].filter(Boolean)
   }
 }
